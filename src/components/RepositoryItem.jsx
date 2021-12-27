21c3c1a6f66b7e5148eb22d4fbf75b2dc103680a
WallysLS <wallys31.souza@gmail.com>
2021-12-27 15:58:18 -0300

    Updated files

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -3,11 +3,13 @@ import React from "react";
 export function RepositoryItem(props){
     return (
         <li>
-        <strong>This repository belongs to  
-            {props.coolProps ?? " Me"}
+        <strong>
+         {props.repository.name ?? "Default"}
         </strong>
-        <p>Forms in react</p>
-        <a href="">Acessar Repositório</a>
+        <p>{props.repository.description}</p>
+        <a href={props.repository.html_url}>
+            Acessar Repositório
+        </a>
         </li>
     )
 }
--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -2,20 +2,24 @@ import React from "react";
 import { RepositoryItem } from "./RepositoryItem"
 import { useState, useEffect } from "react";
 
-//https://api.github.com/users/WallysLS/repos
+
 
 export function RepositoryList(){
     const [repositories,setRepositories] = useState([]);
     
-    useEffect(() => {}, [])
+    useEffect(() => {
+        fetch("https://api.github.com/users/WallysLS/repos")
+        .then(response => response.json())
+        .then(data => setRepositories(data))
+    }, [])
 
     return (
         <section className="repository-list">
             <h1>Lista de Repositórios</h1>
             <ul>
-             <RepositoryItem coolProps = " Naruto" />
-             <RepositoryItem />
-             <RepositoryItem />
+            {repositories.map(repository => {
+                return  <RepositoryItem key={repository.name} repository={repository} />
+            })}
             </ul>
         </section>
     )
